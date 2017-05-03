6b4f423437ab5605aa283de90a927ace90869537
Jake Johnson <jake@fastdivision.com>
2017-05-03 12:22:34 -0700

    Update package details and dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-import os
 from setuptools import setup
+
 setup(
-    name = 'TaxJar',
-    version = '0.0.1',
-    author = 'TaxJar',
-    author_email = 'support@taxjar.com',
-    url = 'https://developers.taxjar.com',
-    description = 'Python client library for the TaxJar API',
-    packages = ['taxjar']
+    name='taxjar',
+    version='0.0.1',
+    description='Sales tax API client for Python',
+    author='TaxJar',
+    author_email='support@taxjar.com',
+    url='https://github.com/taxjar/taxjar-python',
+    packages=['taxjar'],
+    install_requires=[
+        'requests >= 2.13.0'
+    ],
+    tests_require=[
+        'mock'
+    ]
 )
