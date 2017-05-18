b7d8b6bf58bacd6775875e986f9a19c0996d1ffd
Corin Schedler <corin@dubdromic.com>
2017-05-18 14:08:06 -0500

    Clarify float property reasoning

--- taxjar/data/float_property.py
+++ taxjar/data/float_property.py
@@ -1,5 +1,10 @@
 from jsonobject import FloatProperty
 
+# jsonobject's FloatProperty is strict in it's interpretation that JSON floats
+# == float. If there's a "float-like" string - "0.054, for example - it will
+# throw a BadValueError. This subclass just loosens the requirement a bit, and
+# passes any float coercsion issues up to the superclass.
+
 class TaxJarFloatProperty(FloatProperty):
     def selective_coerce(self, obj):
         try:
