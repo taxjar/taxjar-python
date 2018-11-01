be9b30d7dc968d24e066c7c133849fee180f8d95
Jake Johnson <jake@fastdivision.com>
2018-11-01 10:50:59 -0700

    Version 1.6.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='taxjar',
-    version='1.5.0',
+    version='1.6.0',
     description='Sales tax API client for Python',
     author='TaxJar',
     author_email='support@taxjar.com',
     url='https://github.com/taxjar/taxjar-python',
-    download_url='https://github.com/taxjar/taxjar-python/archive/v1.5.0.zip',
+    download_url='https://github.com/taxjar/taxjar-python/archive/v1.6.0.zip',
     packages=['taxjar', 'taxjar.data'],
     install_requires=[
         'requests >= 2.13.0',
